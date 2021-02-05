95f4b40be6575500dcd740d7cabdf4b325d9695e
jup6039 <42753442+jup6039@users.noreply.github.com>
2021-02-04 23:09:17 -0500

    added eslint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-console.log("First web service starting up ...");
+const name = 'fred';
+const car = {
+  make: 'Ford',
+};
+
+// console.log('First web service starting up ...');
 
 // 1 - pull in the HTTP server module
 const http = require('http');
@@ -21,8 +26,7 @@ const indexPage = `
     <p>
       Random Number Web Service - the endpoint is here --> 
       <a href="/random-number">random-number</a> or <a href="/random-number?max=10">random-number?max=10</a>
-    </p>
-	<p>:D</p>
+    </p><p>:D</p>
   </body>
 </html>`;
 
@@ -34,61 +38,56 @@ const errorPage = `
   </head>
   <body>
     <h1>404 - File Not Found!</h1>
-    <p>Check your URL, or your typing!!</p>
-	<p>:-0</p>
+    <p>Check your URL, or your typing!!</p><p>:-0</p>
   </body>
 </html>`;
 
-
 // 6 - this will return a random number no bigger than `max`, as a string
 // we will also doing our query parameter validation here
-const getRandomNumberJSON = (max=1) => {
-	max = Number(max);	// cast max to a Number
-	max = !max ? 1 : max;	// if max is not a number because it is "falsy" NaN, default to 1
-	max = max < 1 ? 1 : max;	// if max is less than 1 default to 1
-	
-	const number = Math.random() * max;
-	const responseObj = {
-		timestamp: new Date(),
-		number: number
-	};
-	return JSON.stringify(responseObj);
-};
+const getRandomNumberJSON = (max = 1) => {
+  let max2 = Number(max); // cast max to a Number
+  max2 = !max2 ? 1 : max2; // if max is not a number because it is "falsy" NaN, default to 1
+  max2 = max2 < 1 ? 1 : max2; // if max is less than 1 default to 1
 
+  const number = Math.random() * max2;
+  const responseObj = {
+    timestamp: new Date(),
+    number,
+  };
+  return JSON.stringify(responseObj);
+};
 
 // 7 - this is the function that will be called every time a client request comes in
 // this time we will look at the `pathname`, and send back the appropriate page
 // note that in this course we'll be using arrow functions 100% of the time in our server-side code
 const onRequest = (request, response) => {
-  //console.log(request.headers);
+  // console.log(request.headers);
   const parsedUrl = url.parse(request.url);
-  const pathname = parsedUrl.pathname;
-  console.log("parsedUrl=", parsedUrl);
-  console.log("pathname=", pathname);
-  
+  const { pathname } = parsedUrl;
+  // console.log('parsedUrl=', parsedUrl);
+  // console.log('pathname=', pathname);
+
   const params = query.parse(parsedUrl.query);
-  const max = params.max;
-  console.log("params=", params);
-  console.log("max=", max);
-  
-  if (pathname == "/") {
-	  response.writeHead(200, { 'Content-Type': 'text/html'});	// send response headers
-	  response.write(indexPage);	// send content
-	  response.end();	// close connection
-  } else if (pathname == "/random-number") {
-	  response.writeHead(200, { 'Content-Type': 'application/json'});	// send response headers
-	  response.write(getRandomNumberJSON(max));	// send content
-	  response.end();	// close connection
+  const { max } = params;
+  // console.log('params=', params);
+  // console.log('max=', max);
+
+  if (pathname === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/html' }); // send response headers
+    response.write(indexPage); // send content
+    response.end(); // close connection
+  } else if (pathname === '/random-number') {
+    response.writeHead(200, { 'Content-Type': 'application/json' }); // send response headers
+    response.write(getRandomNumberJSON(max)); // send content
+    response.end(); // close connection
   } else {
-	  response.writeHead(404, { 'Content-Type': 'text/html'});	// send response headers
-	  response.write(errorPage);	// send content
-	  response.end();	// close connection
+    response.writeHead(404, { 'Content-Type': 'text/html' }); // send response headers
+    response.write(errorPage); // send content
+    response.end(); // close connection
   }
-   
 };
 
-
 // 8 - create the server, hook up the request handling function, and start listening on `port`
-http.createServer(onRequest).listen(port);	// method chaining
+http.createServer(onRequest).listen(port); // method chaining
 
-console.log(`Listening on 127.0.0.1: ${port}`);
+// console.log(`Listening on 127.0.0.1: ${port}`);
